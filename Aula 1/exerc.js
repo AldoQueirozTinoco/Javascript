e33ca881037a0a7299a0f201c8f150674eef2db8
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-11-28 18:31:20 -0300

    Aula 1

---
+++ Aula 1/exerc.js
@@ -0,0 +1,2 @@
+console.log("Me chamo 'ALDO' e estou aprendendo JS as",5+":41 da tarde")
+//Pelo visto posso usar o + para concatenar strings! (Igual ao python!!!)
---
+++ Aula 1/index.js
@@ -0,0 +1,5 @@
+console.log("OKKKKK"); //STRING!!! 
+console.log("OKKKKK de vdd");
+// O java ja separa as linhas toda a vez q vem um console.log
+console.log(1234 , 15.85); // pode botar numero sem aspas!
+// floats e ints sao o mesmo tipo em java (Number)
