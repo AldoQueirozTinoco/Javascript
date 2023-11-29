4e941ec987cc8e6d5df30f61cf2629c7fac92da3
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-11-29 08:17:54 -0300

    Parte de variáveis
    Vou ver as constantes e var dps

---
+++ Aula3/Aula3.js
@@ -0,0 +1,7 @@
+let nome = "Jhon";//Declaração e inicialização de variável
+console.log(nome,"é procurado pela policia!");
+console.log("Ele se chama",nome,"e é um doido varrido!");
+let variavel; //Declaração de variável não-inicializada
+nome = "João";
+console.log("Se mudou para o brasil, e agora se chama",nome);
+console.log(nome,"continua sendo procurado pela policia");
