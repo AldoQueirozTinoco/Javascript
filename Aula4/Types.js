b2ee323cb9eafeff715c3e5224c6daf4943b08a0
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-01 21:37:33 -0300

    Create Types.js

---
+++ Aula4/Types.js
@@ -0,0 +1,11 @@
+const nome1 = "Aldo" //String
+const nome2 = 'Aldo' //String
+const nome3 = `Aldo` //String que suporta inserções
+const num1=1, num2=2
+let nomeAluno; // Undefined ->Não aponta para local nenhum na memoria
+let nomeAluno1 = null;// Nulo ->Não aponta para local nenhum na memoria
+// null define que um valor é nulo e undefined é por que não foi definido
+const bollean= true,bollean2= false;
+const aprovado = bollean;
+const reprovado = bollean2;
+console.log(typeof(nome1),nome1)
