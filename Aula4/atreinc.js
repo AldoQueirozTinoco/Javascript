90b484148e520885d2b82d3160075798ef336a69
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-01 22:32:13 -0300

    Create atreinc.js
    Tava cansado ontem, hj botei o de ontem e o de hj

---
+++ Aula4/atreinc.js
@@ -0,0 +1,31 @@
+//aritimeticos
+// + adicao/concatenacao
+// - subtracao
+// *multiplicacao
+// ** potencializacao
+// / resultado da divisao
+// % resto da divisao
+//soma e conc
+const num1 = 5
+const num2 = 2
+console.log(num1+num2) // soma
+const num3 = "5"
+console.log(num1 + num3) //concatenar
+
+// ++ e +=
+
+let contador=1
+contador++ // ou ++contador
+contador++
+contador++
+console.log(contador) // 4
+contador += num1 //contador = contador + num1
+console.log(contador)/* 4 + 5*/
+
+//wtf java
+const a =10
+const b ="10"
+console.log(a*b)//100 ????????
+//conversion
+const c = parseInt('10');
+console.log(a*c) //100
