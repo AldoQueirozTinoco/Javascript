1b0e421cf5f00291e2d277bab3779d63114d4d71
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-04 19:55:01 -0300

    Pasta de brincadeiras
    Preciso de um local para brincar

---
+++ Testes/test1.js
@@ -0,0 +1,9 @@
+
+alert("Bom thia!");
+let prompts = confirm("Uhhhh, boolean, você compila ou não?")
+if (prompts ==true ) {
+    console.log("Bom thia! esse codigo compila!")
+    document.body.innerHTML += ("Este código está compilado!\nBom thia!")
+}else{
+console.log("trivial, nem compila.")
+document.body.innerHTML+= "Trivial, nem compila"}
