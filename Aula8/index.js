a0dc07125912f8ab556aa69fae3652aecdf7730b
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-05 16:35:07 -0300

    Aula de math
    Dps eu faco os exercicios

---
+++ Aula8/index.js
@@ -0,0 +1,12 @@
+let num1 = 10.54
+console.log(Math.floor(num1))//arredonda down
+console.log(Math.ceil(num1))//arredonda up
+console.log(Math.round(num1))//arredondamento
+console.log(Math.max(1,2,3,4,5,6,-21,10))
+console.log(Math.round(Math.random()*(10-5)+5))//entre 10 e 5
+console.log(Math.pow(2,10))
+//ou
+console.log(2**10)//+ Facil Xddd
+console.log(Math.sqrt(9))
+console.log(100/0)//infinity!!! Eh um calculo True
+console.log(!!(100/0))//ta vendo?
