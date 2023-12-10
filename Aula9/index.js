962e5ceac1d4a9fbd42c7e63254f027ed1844ef4
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-09 23:23:14 -0300

    Aula de arrays 1/2
    Deu já por hj

---
+++ Aula9/index.js
@@ -0,0 +1,16 @@
+const alunos = ["luiz","joao","maria"]
+/*Tentar manter os arrays consisos,
+Não utilizar diversos tipos de dados nele*/
+console.log(alunos)
+//                 0      1      2
+//const alunos = ["luiz","joao","maria"]
+
+console.log(alunos[0])
+console.log(alunos[0][0])
+alunos[alunos.length]="Jose" //Detalhe, Sizeof eh em C xdddd
+//alunos[alunos.length]="Fagner"
+//alunos[alunos.length]="Fagner"
+alunos.push("Fagner") //Melhor q botar o .length
+alunos.unshift("Luiza")
+console.log(alunos)
+
