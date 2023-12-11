ee5d4742a9f497fec6f115261f56a9325fea8b57
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-11 19:10:24 -0300

    Fim da aula de vetores
    Cansadissimo

--- Aula9/index.js
+++ Aula9/index.js
@@ -7,10 +7,13 @@ console.log(alunos)
 
 console.log(alunos[0])
 console.log(alunos[0][0])
-alunos[alunos.length]="Jose" //Detalhe, Sizeof eh em C xdddd
-//alunos[alunos.length]="Fagner"
-//alunos[alunos.length]="Fagner"
-alunos.push("Fagner") //Melhor q botar o .length
-alunos.unshift("Luiza")
+alunos[alunos.length]="Jose" //Coloca o elemento descrito no final do array(alunos.length = 3)
+alunos.push("Fagner") //add elemento no final
+alunos.unshift("Luiza")//add elemento no primeiro indice
 console.log(alunos)
+const removido = alunos.pop()//remove o ultimo elemento
+const removido2 = alunos.shift()//remove o primeiro elemento
+console.log(alunos)
+console.log(alunos.slice(0,2))
+console.log(alunos instanceof Array)
 
