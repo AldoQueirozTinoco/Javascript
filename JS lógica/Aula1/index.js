1ae103049f8eca7436b37db81c319a11053f51ec
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-24 09:33:41 -0300

    Aula de operadores de comp

---
+++ JS lógica/Aula1/index.js
@@ -0,0 +1,17 @@
+/*
+== igualdade (valor)
+=== igualdade estrita
+!= diferente (valor)
+!==diferente estrito
+______________________
+O resto é igual a C
+>, <, >=, <=.
+*/
+const comp = 1<2;
+console.log(1<2); //True, mto true
+console.log(comp)
+const num1 = 10
+const num2 = '10'
+console.log(num1==num2)// wtf
+console.log(num1===num2)// ah sim
+console.log(num1!==num2)
